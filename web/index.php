c8a24c99318af03e5ef74c3512b1c6fc19c933c3
Volodymyr Khorolskyi <Volodymyr.Khorolskyi@stepstone.com>
2020-07-11 16:00:52 +0200

    clean up

--- web/index.php
+++ web/index.php
@@ -1,25 +1,44 @@
-<?php
+<?php declare(strict_types=1);
 
 require('../vendor/autoload.php');
 
 $app = new Silex\Application();
 $app['debug'] = true;
 
-// Register the monolog logging service
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
+    'monolog.logfile' => 'php://stderr',
 ));
-
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
-
-// Our web handlers
 
 $app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
+    return getResponse('application up and running');
+});
+
+$app->post('/1SYu4H4BG2SA0CL5i9Gm', function() use($app) {
+
+    $request = json_decode(file_get_contents('php://input'));
+
+    if(!$request) {
+        return getResponse('empty request');
+    }
+
+    if($request->secret !== getenv('VK_SECRET_TOKEN') && $request->type !== 'confirmation') {
+        return getResponse('invalid secret and request type');
+    }
+
+    switch ($request->type) {
+        case 'confirmation':
+            return '1c96ca0e';
+            break;
+        case 'message_new':
+            break;
+    }
+
+    return getResponse('unknown request');
 });
 
 $app->run();
+
+function getResponse(string $message): string
+{
+    return json_encode(array('message' => $message));
+}
